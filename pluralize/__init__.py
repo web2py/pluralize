2de5bc14f91498add8206bb55b1165e45308dde2
Massimo Di Pierro <massimo.dipierro@gmail.com>
2021-07-16 00:49:38 -0700

    version 0.1.7 enures idempotency

--- pluralize/__init__.py
+++ pluralize/__init__.py
@@ -4,9 +4,9 @@ import json
 import threading
 import ast
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
-re_language = re.compile('^\w\w(-\w+)*.json$')
+re_language = re.compile("^\w\w(-\w+)*.json$")
 
 
 class lazyT(object):
@@ -42,7 +42,6 @@ class lazyT(object):
 
 
 class Translator(object):
-
     def __init__(self, folder=None):
         """
         creates a translator object loading languages and pluralizations from translations/en-US.py files
@@ -56,6 +55,7 @@ class Translator(object):
         self.local.tag = None
         self.local.language = None
         self.missing = set()
+        self.folder = folder
         if folder:
             self.load(folder)
 
@@ -64,24 +64,26 @@ class Translator(object):
         self.languages = {}
         for filename in os.listdir(folder):
             if re_language.match(filename):
-                with open(os.path.join(folder, filename), 'r') as fp:
+                with open(os.path.join(folder, filename), "r") as fp:
                     self.languages[filename[:-5].lower()] = json.load(fp)
 
-    def save(self, folder):
+    def save(self, folder=None):
         """save the loaded translation files"""
+        folder = folder or self.folder
         for key in self.languages:
-            filename = '%s.json' % key
-            with open(os.path.join(folder, filename), 'w') as fp:
+            filename = "%s.json" % key
+            with open(os.path.join(folder, filename), "w") as fp:
                 json.dump(self.languages[key], fp, sort_keys=True, indent=4)
 
-    def select(self, accepted_languages='fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5'):
+    def select(self, accepted_languages="fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5"):
         """given appected_langauges string from HTTP header, picks the best match"""
         if isinstance(accepted_languages, str):
-            accepted_languages = [tag.split(
-                ';')[0].strip() for tag in accepted_languages.split(',')]
+            accepted_languages = [
+                tag.split(";")[0].strip() for tag in accepted_languages.split(",")
+            ]
             for tag in accepted_languages:
-                for k in range(tag.count('-'), 0, -1):
-                    subtag = '-'.join(tag.split('-')[:k])
+                for k in range(tag.count("-"), 0, -1):
+                    subtag = "-".join(tag.split("-")[:k])
                     if not subtag in accepted_languages:
                         accepted_languages.append(subtag)
         self.local.tag = None
@@ -94,12 +96,14 @@ class Translator(object):
 
     def __call__(self, text):
         """retuns a lazyT object"""
+        if isinstance(text, lazyT):
+            return text
         return lazyT(self._translator, text)
 
     def _translator(self, text, **kwargs):
         """translates/pluralizes"""
         if self.local.language:
-            n = kwargs.get('n', 1)
+            n = kwargs.get("n", 1)
             translations = self.local.language.get(text)
             if translations is None:
                 self.missing.add(text)
@@ -108,18 +112,21 @@ class Translator(object):
                 text = translations[str(k)].format(**kwargs)
         return text.format(**kwargs)
 
-    def find_matches(self, folder, name='T', extensions=['py', 'js', 'html']):
+    @staticmethod
+    def find_matches(folder, name="T", extensions=["py", "js", "html"]):
         """finds all strings in files in folder needing translations"""
         matches_found = set()
-        re_string_t = (r'(?<=[^\w]%s\()(?P<name>'
+        re_string_t = (
+            r"(?<=[^\w]%s\()(?P<name>"
             r"[uU]?[rR]?(?:'''(?:[^']|'{1,2}(?!'))*''')"
             r"|(?:'(?:[^'\\]|\\.)*')"
             r'|(?:"""(?:[^"]|"{1,2}(?!"))*""")'
-            r'|(?:"(?:[^"\\]|\\.)*"))') % name
+            r'|(?:"(?:[^"\\]|\\.)*"))'
+        ) % name
         regex_t = re.compile(re_string_t)
         for root, dirs, files in os.walk(folder):
             for name in files:
-                if name.split('.')[-1] in extensions:
+                if name.split(".")[-1] in extensions:
                     path = os.path.join(root, name)
                     with open(path) as fp:
                         data = fp.read()
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -3,21 +3,43 @@ from pluralize import Translator
 
 
 class TestPluralization(unittest.TestCase):
-    def test_simple(self):
+    def setUp(self):
         T = Translator()
-        T.update_languages(T.find_matches('./'))
-        dog = T('dog')
-        T.languages = {'it': {'dog': {'0': 'no cane', '1': 'un cane',
-                                      '2': 'due cani', '3': 'alcuni cani', '10': 'tanti cani'}}}
-        T.select('en;q=0.9,it-IT;q=0.1')
-        self.assertEqual(str(dog.format(n=0)), 'no cane')
-        self.assertEqual(str(dog.format(n=1)), 'un cane')
-        self.assertEqual(str(dog.format(n=2)), 'due cani')
-        self.assertEqual(str(dog.format(n=3)), 'alcuni cani')
-        self.assertEqual(str(dog.format(n=5)), 'alcuni cani')
-        self.assertEqual(str(dog.format(n=100)), 'tanti cani')
+        T.update_languages(T.find_matches("./"))
+        T.languages = {
+            "it": {
+                "dog": {
+                    "0": "no cane",
+                    "1": "un cane",
+                    "2": "due cani",
+                    "3": "alcuni cani",
+                    "10": "tanti cani",
+                }
+            }
+        }
+        T.select("en;q=0.9,it-IT;q=0.1")
+        self.T = T
 
-        plus = T('plus')
-        T.languages['it']['plus'] = {'0': "piu'"}
+    def test_simple(self):
+        T = self.T
+        dog = T("dog")
+        self.assertEqual(str(dog.format(n=0)), "no cane")
+        self.assertEqual(str(dog.format(n=1)), "un cane")
+        self.assertEqual(str(dog.format(n=2)), "due cani")
+        self.assertEqual(str(dog.format(n=3)), "alcuni cani")
+        self.assertEqual(str(dog.format(n=5)), "alcuni cani")
+        self.assertEqual(str(dog.format(n=100)), "tanti cani")
+
+        plus = T("plus")
+        T.languages["it"]["plus"] = {"0": "piu'"}
         self.assertEqual(
-            dog + ' ' + plus + ' ' + dog.format(n=2), "un cane piu' due cani")
+            dog + " " + plus + " " + dog.format(n=2), "un cane piu' due cani"
+        )
+
+    def test_idempotency(self):
+        T = self.T
+        text = "dog"
+        a = T(text)
+        b = T(a)
+        c = T(b)
+        self.assertEqual(str(c.format(n=1)), "un cane")
